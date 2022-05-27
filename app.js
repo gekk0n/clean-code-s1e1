64d96c0201405cf25ef09af9a4b8d1c962b9846b
Daniel Samko <gekk0n.ua@gmail.com>
2022-05-27 18:51:57 +0300

    feat: add 'alt' attribute according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "delete-img"
+    deleteButtonImg.alt = 'delete-button'
     deleteButton.appendChild(deleteButtonImg);
 
 
